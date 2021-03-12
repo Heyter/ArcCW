01ba1b304f43e9ea28c0d6f60bb7dd7730a62918
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2021-03-13 00:30:20 +0300

    Update sh_attachments.lua

--- lua/arccw/shared/sh_attachments.lua
+++ lua/arccw/shared/sh_attachments.lua
@@ -50,11 +50,9 @@ function ArcCW:SlotAcceptsAtt(slot, wep, att)
 
     if atttbl.Hidden or atttbl.Blacklisted or ArcCW.AttachmentBlacklistTable[att] then return false end
 
-	if (wep.Owner) then
-        if (atttbl.NotForNPC or atttbl.NotForNPCs) and wep.Owner:IsNPC() then
-            return false
-        end
-	end
+    if (atttbl.NotForNPC or atttbl.NotForNPCs) and wep.Owner and wep.Owner:IsNPC() then
+        return false
+    end
 
     if wep.RejectAttachments and wep.RejectAttachments[att] then return false end
 
